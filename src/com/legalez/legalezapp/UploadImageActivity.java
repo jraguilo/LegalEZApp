d498db8971eb53c44b49fbca6e21dcd0af5f411c
jraguilo <johnreyaguilo@gmail.com>
2014-03-08 04:34:36 -0800

    Merging stuff and fixing stuff

--- src/com/legalez/legalezapp/UploadImageActivity.java
+++ src/com/legalez/legalezapp/UploadImageActivity.java
@@ -1,22 +1,46 @@
 package com.legalez.legalezapp;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class UploadImageActivity extends Activity {
 
+    private static final int SELECT_PICTURE = 0;
+    private EditText mImageName;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_image);
-    }
+        
+        mImageName = (EditText) findViewById(R.id.image_name_field);
+        
+        final Button selectImageButton = (Button) findViewById(R.id.select_image_button);
+        selectImageButton.setOnClickListener(new OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                Intent newInt = new Intent();
+                newInt.setType("image/*");
+                newInt.setAction(Intent.ACTION_PICK);
+                startActivityForResult(newInt, SELECT_PICTURE);
+            }
+        });
+    }
+    
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.upload_image, menu);
-        return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+    
+        if (resultCode == RESULT_OK) {
+            Uri selectedImage = imageReturnedIntent.getData();
+            mImageName.setText(selectedImage.toString());
+        }
     }
 
 }
